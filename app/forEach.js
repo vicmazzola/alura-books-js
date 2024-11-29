48cfb1b2a93cf8f8308b1f886a10dc1cd3437076
Victor Mazzola <victormazzola2019@gmail.com>
2024-11-29 07:53:29 -0300

    add availability

--- app/forEach.js
+++ app/forEach.js
@@ -3,9 +3,10 @@ const elementInsertBooks = document.getElementById('books')
 function showBooksOnScreen(booksList) {
     elementInsertBooks.innerHTML = ''
     booksList.forEach(book => {
+    let availability = book.quantidade > 0 ? 'book__image' : 'book__image unavailable'
         elementInsertBooks.innerHTML += `
           <div class="book">
-          <img class="book__image" src="${book.imagem}" alt="${book.alt}" />
+          <img class="${availability}" src="${book.imagem}" alt="${book.alt}" />
           <h2 class="book__title"> ${book.titulo} </h2>
           <p class="book__description">${book.autor}</p>
           <p class="book__price" id="preco">R$${book.preco.toFixed(2)}</p>
@@ -15,3 +16,4 @@ function showBooksOnScreen(booksList) {
   </div>`
     })
 }
+
