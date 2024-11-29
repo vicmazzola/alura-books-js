f25529f50bd58e4726b0419cb8b233dbd0ced2fd
Victor Mazzola <victormazzola2019@gmail.com>
2024-11-29 07:58:00 -0300

    add availability filter

--- app/filter.js
+++ app/filter.js
@@ -4,7 +4,7 @@ buttons.forEach(btn => btn.addEventListener('click', filterBooks))
 function filterBooks() {
     const btnElement = document.getElementById(this.id)
     const category = btnElement.value
-    let filteredBooks = books.filter(book => book.categoria == category)
+    let filteredBooks = category == 'available' ? books.filter(book => book.quantidade > 0) : books.filter(book => book.categoria == category)
     showBooksOnScreen(filteredBooks);
     console.table(filteredBooks);
 
